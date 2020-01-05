cccfcf232ad7f2cb55cd0cd6cbb50809cb7c91ae
Omar El Hefny <o.hefny@gmail.com>
2020-01-06 00:17:21 +0200

    init git repo

---
+++ app/src/main/java/com/example/swvlmovies/modules/movies/presentation/MoviesActivity.kt
@@ -0,0 +1,13 @@
+package com.example.swvlmovies.modules.movies.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.swvlmovies.R
+
+class MoviesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
