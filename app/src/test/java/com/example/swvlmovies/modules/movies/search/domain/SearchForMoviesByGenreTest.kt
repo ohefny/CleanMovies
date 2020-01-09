4cbc9bd46975e53b3939b66bdfa44d9816bfa8a0
Omar El Hefny <o.hefny@gmail.com>
2020-01-10 01:06:14 +0200

    test: add more tests on group count and size of movies

--- app/src/test/java/com/example/swvlmovies/modules/movies/search/domain/SearchForMoviesByGenreTest.kt
+++ app/src/test/java/com/example/swvlmovies/modules/movies/search/domain/SearchForMoviesByGenreTest.kt
@@ -20,17 +20,18 @@ class SearchForMoviesByGenreTest {
     lateinit var validateGenreSearch: ValidateGenreSearch
     lateinit var searchForMoviesByGenre: SearchForMoviesByGenre
     private val genresList = listOf("action", "drama", "war", "crime")
+    val movieClone=Movie(emptyList(), genresList, 1, "some title10", 1992)
     private val movies = listOf(
-        Movie(emptyList(), genresList, 1, "some title10", 1992)
-        , Movie(emptyList(), genresList, 2, "some title9", 1992)
-        , Movie(emptyList(), genresList, 3, "some title8", 1992)
-        , Movie(emptyList(), genresList, 4, "some title7", 1992)
-        , Movie(emptyList(), genresList, 5, "some title6", 1992)
-        , Movie(emptyList(), genresList, 6, "some title5", 1992)
-        , Movie(emptyList(), genresList, 6, "some title4", 1992)
-        , Movie(emptyList(), genresList, 6, "some title3", 1992)
-        , Movie(emptyList(), genresList, 6, "some title2", 1992)
-        , Movie(emptyList(), genresList, 6, "some title1", 1992)
+        movieClone
+        , movieClone.copy(rating = 2,title = movieClone.title+"1")
+        , movieClone.copy(rating = 3,title = movieClone.title+"2")
+        , movieClone.copy(rating = 4,title = movieClone.title+"3")
+        , movieClone.copy(rating = 5,title = movieClone.title+"4")
+        , movieClone.copy(rating = 6,title = movieClone.title+"5")
+        , movieClone.copy(rating = 6,title = movieClone.title+"6")
+        , movieClone.copy(rating = 6,title = movieClone.title+"7")
+        , movieClone.copy(rating = 6,title = movieClone.title+"8")
+        , movieClone.copy(rating = 6,title = movieClone.title+"9")
     )
 
     @Before
@@ -63,28 +64,50 @@ class SearchForMoviesByGenreTest {
 
     @Test
     fun `given listed genre when SearchForMoviesByGenre_build is called then only limited movies is emitted`() {
-        val movieGroup = searchForMoviesByGenre.build("action")
+        val firstYear = searchForMoviesByGenre.build("action")
             .test()
             .values()[0][0]//take first group
-        assertEquals(PER_YEAR_LIMIT, movieGroup.movies.size.toLong())
+        assertEquals(PER_YEAR_LIMIT, firstYear.movies.size.toLong())
     }
 
     @Test
     fun `given listed genre when SearchForMoviesByGenre_build is called then only limited top rated movies is emitted`() {
-        val movieGroup = searchForMoviesByGenre.build("action")
+        val firstYear = searchForMoviesByGenre.build("action")
             .test()
             .values()[0][0]//take first group
         for (i in 0..4)
-            assertEquals(6, movieGroup.movies[i].rating)
+            assertEquals(6, firstYear.movies[i].rating)
     }
 
     @Test
     fun `given movies with same year and more than the limit when SearchForMoviesByGenre_build is called then only one group is emitted for this year`() {
-        val movieGroups = searchForMoviesByGenre.build("action")
+        val years = searchForMoviesByGenre.build("action")
             .test()
-            .values()[0]//take first group
-        val distinctGroups = movieGroups.distinctBy { it.year }
-        assertEquals(movieGroups.size, distinctGroups.size)
+            .values()[0]
+        val distinctGroups = years.distinctBy { it.year }
+        assertEquals(years.size, distinctGroups.size)
+    }
+    @Test
+    fun `given movies with differnt years when SearchForMoviesByGenre_build is called then groups with the same year number are emitted`() {
+        val moviesWithDifferentYears= mutableListOf<Movie>().apply {addAll(movies)}
+        moviesWithDifferentYears.add(movieClone.copy(year = 2000))
+        moviesWithDifferentYears.add(movieClone.copy(year = 2002)) //now years are 1998 2000 2002
+        whenever(repository.getMoviesWithGenre(any())).thenReturn(Flowable.fromIterable(moviesWithDifferentYears).toList())
+        val years = searchForMoviesByGenre.build("action")
+            .test()
+            .values()[0]
+        assertEquals(3,years.size)
+    }
+    @Test
+    fun `given movies of year less than limit when SearchForMoviesByGenre_build is called then all those movies are emitted`() {
+        val moviesWithFewerMovies= mutableListOf<Movie>()
+        moviesWithFewerMovies.add(movieClone.copy(year = 2000))
+        moviesWithFewerMovies.add(movieClone.copy(year = 2000)) //only 2 items of year 2000
+        whenever(repository.getMoviesWithGenre(any())).thenReturn(Flowable.fromIterable(moviesWithFewerMovies).toList())
+        val year2000Group = searchForMoviesByGenre.build("action")
+            .test()
+            .values()[0][0]//first group
+        assertEquals(2,year2000Group.movies.size)
     }
 
 }
