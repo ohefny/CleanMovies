47dd5627b54c18b242ba8f62fc0a31d472f53fb1
Omar El Hefny <o.hefny@gmail.com>
2020-01-10 16:12:02 +0200

    refactor: extract MoviesOfYear to seperate class and change it's name -fix: sort after grouping not before

--- app/src/main/java/com/example/swvlmovies/modules/movies/search/domain/SearchForMoviesByGenre.kt
+++ app/src/main/java/com/example/swvlmovies/modules/movies/search/domain/SearchForMoviesByGenre.kt
@@ -1,32 +1,34 @@
 package com.example.swvlmovies.modules.movies.search.domain
 
 import com.example.swvlmovies.base.domian.SingleUseCase
-import com.example.swvlmovies.modules.movies.search.domain.enitiy.Movie
+import com.example.swvlmovies.modules.movies.search.domain.enitiy.MoviesOfYear
 import io.reactivex.Single
+import timber.log.Timber
 import javax.inject.Inject
 
 class SearchForMoviesByGenre @Inject constructor(
     private val repository: MoviesSearchRepository,
     private val validateGenreSearch: ValidateGenreSearch
-) :
-    SingleUseCase<String, List<SearchForMoviesByGenre.MoviesGroupByYear>>() {
-    override fun build(params: String): Single<List<MoviesGroupByYear>> {
+) : SingleUseCase<String, List<MoviesOfYear>>() {
+    override fun build(params: String): Single<List<MoviesOfYear>> {
         return validateGenreSearch.build(params)
             .flatMap { getMovies(params) }
     }
 
-    private fun getMovies(params: String): Single<MutableList<MoviesGroupByYear>> {
+    private fun getMovies(params: String): Single<List<MoviesOfYear>> {
         return repository.getMoviesWithGenre(params).flattenAsFlowable { it }
-            .sorted { o1, o2 -> o2.rating.compareTo(o1.rating) } //descending sorting
             .groupBy { it.year }
             .distinct { it.key }
-            .flatMapSingle {it.take(PER_YEAR_LIMIT).toList() }
-            .map { MoviesGroupByYear(it[0].year, it) }
-            .sorted { o1, o2 -> o1.year.compareTo(o2.year) }
+            .flatMapSingle {
+                it.sorted { movie, movie2 -> movie2.rating.compareTo(movie.rating) } //descending sorting
+                .take(PER_YEAR_LIMIT)
+                .toList()
+            }
+            .map { MoviesOfYear(it[0].year, it) }
+            .doOnNext { Timber.d("getMovies: $it") }
             .toList()
     }
 
-    data class MoviesGroupByYear(val year: Int, val movies: List<Movie>)
 
     companion object {
         const val PER_YEAR_LIMIT = 5L
---
+++ app/src/main/java/com/example/swvlmovies/modules/movies/search/domain/enitiy/MoviesOfYear.kt
@@ -0,0 +1,3 @@
+package com.example.swvlmovies.modules.movies.search.domain.enitiy
+
+data class MoviesOfYear(val year: Int, val movies: List<Movie>)
