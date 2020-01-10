b0d1ce5b752a44d17654e9f5bd14006b0851259c
Omar El Hefny <o.hefny@gmail.com>
2020-01-10 23:09:19 +0200

    feat: add navigation from search fragment to details fragment

--- app/src/main/java/com/example/swvlmovies/modules/movies/presentation/MoviesActivity.kt
+++ app/src/main/java/com/example/swvlmovies/modules/movies/presentation/MoviesActivity.kt
@@ -5,13 +5,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import com.example.swvlmovies.R
+import com.example.swvlmovies.modules.movies.features.details.presentation.MovieDetailsFragment
 import com.example.swvlmovies.modules.movies.features.search.domain.enitiy.Movie
 import com.example.swvlmovies.modules.movies.features.search.presentaion.MoviesSearchFragment
 import dagger.android.support.DaggerAppCompatActivity
 
 class MoviesActivity : DaggerAppCompatActivity(),MoviesSearchFragment.MovieSelectedListener {
-    override fun onMovieSelected(movie: Movie) {
-        Toast.makeText(this,movie.title,Toast.LENGTH_LONG).show()
+    override fun onMovieSelected(movie: Movie){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainer,MovieDetailsFragment.newInstance(movie.title,movie.year))
+            .addToBackStack(null)
+            .commit()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +24,9 @@ class MoviesActivity : DaggerAppCompatActivity(),MoviesSearchFragment.MovieSelec
         setContentView(R.layout.activity_movies_search)
         if(savedInstanceState==null){
             //todo check if tablet add both fragments
-            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer,MoviesSearchFragment.newInstance())
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragmentContainer,MoviesSearchFragment.newInstance(),MoviesSearchFragment::class.java.name)
                 .commit()
         }
     }
