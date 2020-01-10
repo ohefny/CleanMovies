7c39bd04bd00c7abab87d90709de7c93e3bc489d
Omar El Hefny <o.hefny@gmail.com>
2020-01-10 23:08:42 +0200

    test: add test for getMovieByID and getMovidePhotos

--- app/src/test/java/com/example/swvlmovies/modules/movies/features/details/domain/GetMovieDetailsTest.kt
+++ app/src/test/java/com/example/swvlmovies/modules/movies/features/details/domain/GetMovieDetailsTest.kt
@@ -1,42 +1,51 @@
 package com.example.swvlmovies.modules.movies.features.details.domain
 
-import android.content.pm.ModuleInfo
-import com.example.swvlmovies.modules.movies.features.search.domain.MoviesSearchRepository
-import com.example.swvlmovies.modules.movies.features.search.domain.ValidateGenreSearch
+import com.example.swvlmovies.modules.movies.features.details.domain.entity.MovieIDParam
 import com.example.swvlmovies.modules.movies.features.search.domain.enitiy.Movie
 import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
 import io.reactivex.Flowable
-import org.junit.Assert.*
+import io.reactivex.Single
 import org.junit.Before
 import org.junit.Test
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
 
-class GetMoviePhotosTest{
+class GetMovieDetailsTest{
 
     @Mock
     lateinit var repository: MovieDetailsRepository
     @InjectMocks
-    lateinit var getMoviePhotos: GetMoviePhotos
-    private val movie = Movie(cast = emptyList(), genres = emptyList(), rating = 2, title = "", year = 2019)
+    lateinit var getMovieDetails: GetMovieDetails
+    private val movie = Movie(cast = emptyList(), genres = emptyList(), rating = 2, title = "title", year = 2020)
+    private val movieID = MovieIDParam(title = "title", year = 2020)
+
     private val photosList = listOf("photo1","photo2","photo3","photo4")
     @Before
     @Throws(Exception::class)
     fun setUp() {
         MockitoAnnotations.initMocks(this)
-        whenever(repository.getMoviePhotos(any())).thenReturn(Flowable.fromIterable(photosList))
+        whenever(repository.getMoviePhotos(any())).thenReturn(Single.just(photosList))
+        whenever(repository.getMovieFromID(any())).thenReturn(Single.just(movie))
+
     }
     @Test
-    fun `when GetMoviePhotos_build is called then repository_getMoviesPhotos is called `(){
-        getMoviePhotos.build(movie).test()
+    fun `when GetMovieDetails_build is called then repository_getMoviesPhotos is called `(){
+        getMovieDetails.build(movieID).test()
         verify(repository).getMoviePhotos(any())
     }
     @Test
-    fun `when GetMoviePhotos_build is called then the same photos from repo is emitted `(){
-        getMoviePhotos.build(movie).test().assertValue(photosList)
+    fun `when GetMovieDetails_build is called then repository_getMovieFromID is called `(){
+        getMovieDetails.build(movieID).test()
+        verify(repository).getMovieFromID(any())
+    }
+    @Test
+    fun `when GetMovieDetails_build is called then the same photos from repo is emitted `(){
+        getMovieDetails.build(movieID).test().assertValue{
+            it.photosUrl==photosList
+        }
 
     }
 }
